9ab6702b05e0e9df0179dfbc4ad6ba8a5cb17955
Borte Coder <abdulkerimdulger@hotmail.com>
2020-04-18 19:00:01 +0300

    chart view için responsive iyileştirme yapıldı.

--- src/components/ChartPage/ChartPage.vue
+++ src/components/ChartPage/ChartPage.vue
@@ -1,17 +1,24 @@
 <template>
     <div class="ChartPage">
-        <div class="col-md-12 text-center">
-            <h3>Selected Countries Comparable Line Chart</h3>
-            <span class="badge badge-info mr-1 p-2 d-none" v-for="country in selectedCountry" :key="country">#{{country}}</span>
-            <p class="text-left">
-                <router-link to="/">
+        <div class="row text-center p-1">
+            <div class="col-lg-3 col-md-3 col-sm-2 col-2">
+                <p class="text-left">
+                    <router-link to="/">
                 <span class="btn btn-warning">
                     <font-awesome-icon icon="share" rotation="180" flip="horizontal"/>
                     <span class="d-lg-block d-md-block d-sm-none d-none">Back to Country List</span>
                 </span>
-                </router-link>
-            </p>
+                    </router-link>
+                </p>
+            </div>
+            <div class="col-lg-9 col-md-9 col-sm-10 col-10">
+                <h3 class="d-lg-block d-md-block d-sm-none d-none">Selected Countries Comparable Line Chart</h3>
+                <h5 class="d-lg-none d-md-none d-sm-block d-block">Selected Countries Comparable Line Chart</h5>
+            </div>
         </div>
+        <span class="badge badge-info mr-1 p-2 d-none" v-for="country in selectedCountry"
+              :key="country">#{{country}}</span>
+
         <hr/>
         <LineChart/>
     </div>
@@ -19,14 +26,13 @@
 
 <script>
     import LineChart from "@/components/LineChart/LineChart";
+
     export default {
         components: {LineChart},
-        props: {
-
-        },
-        data:function () {
+        props: {},
+        data: function () {
             return {
-                selectedCountry:[]
+                selectedCountry: []
             };
         },
         created() {
--- src/components/LineChart/LineChart.vue
+++ src/components/LineChart/LineChart.vue
@@ -1,21 +1,30 @@
 <template>
     <div class="LineChart">
-        <div class="text-center chartGroupButton pt-2">
-            <button class="col-md-2 col-sm-3 col-4 btn btn-success p-2 mr-1" @click="drawChart('R')">
-                <font-awesome-icon icon="running"/>
-                Recovered Chart
-            </button>
+        <div class="row flex-row-reverse chartGroupButton mr-2 ml-2">
+            <div class="col-lg-2 col-md-4 col-sm-4 col-4 p-1">
+                <button class="col-md-12 btn btn-success" @click="drawChart('R')">
+                    <font-awesome-icon icon="running"/>
+                    <p class="mb-0">Recovered Case</p>
+                </button>
+            </div>
 
-            <button class="col-md-2 col-sm-3 col-4 btn btn-warning mr-1 p-2" @click="drawChart('C')">
-                <font-awesome-icon icon="procedures"/>
-                Confirmed Chart
-            </button>
+            <div class="col-lg-2 col-md-4 col-sm-4 col-4 p-1">
+                <button class="col-md-12 btn btn-warning" @click="drawChart('C')">
+                    <font-awesome-icon icon="procedures"/>
+                    <p class="mb-0">Confirmed Case</p>
+                </button>
+            </div>
 
-            <button class="col-md-2 col-sm-3 col-3 btn btn-danger mr-1 p-2" @click="drawChart('D')">
-                <font-awesome-icon icon="skull-crossbones"/>
-                Death Chart
-            </button>
-            <p class="small d-lg-none d-sm-none d-md-none" v-if="isLangTr">Grafiği daha iyi görebilmek için ekranı döndürün.</p>
+            <div class="col-lg-2 col-md-4 col-sm-4 col-4 p-1">
+                <button class="col-md-12 btn btn-danger" @click="drawChart('D')">
+                    <font-awesome-icon icon="skull-crossbones"/>
+                    <p class="mb-0">Death Case</p>
+                </button>
+            </div>
+        </div>
+        <div>
+            <p class="small d-lg-none d-sm-none d-md-none" v-if="isLangTr">Grafiği daha iyi görebilmek için ekranı
+                döndürün.</p>
             <p class="small d-lg-none d-sm-none d-md-none" v-else>Rotate the screen to see the chart better.</p>
             <div class="row canvasDiv p-5">
                 <canvas id="line-chart" width="800" height="520"/>
@@ -27,24 +36,23 @@
 <script>
     import $ from 'jquery'
     import Chart from 'chart.js';
-    export default {
-        props: {
 
-        },
-        data:function(){
+    export default {
+        props: {},
+        data: function () {
             return {
-                countryItem:[],
-                countries:[]
+                countryItem: [],
+                countries: []
             }
         },
-        computed:{
+        computed: {
             isLangTr: function () {
                 return navigator.language.split("-")[0] == "tr";
             }
         },
         created() {
         },
-        mounted(){
+        mounted() {
 
             this.countries = this.$route.params.countryStr.split("-");
             fetch("https://pomber.github.io/covid19/timeseries.json")
@@ -75,9 +83,9 @@
                         var item = this.countryItem[country][index];
 
                         var parsedItemDate = item.date.split("-");
-                        if (parsedItemDate[1].length==1) parsedItemDate[1]="0"+parsedItemDate[1];
-                        if (parsedItemDate[2].length==1) parsedItemDate[2]="0"+parsedItemDate[2];
-                        item.date = parsedItemDate[0]+"-"+parsedItemDate[1]+"-"+parsedItemDate[2];
+                        if (parsedItemDate[1].length == 1) parsedItemDate[1] = "0" + parsedItemDate[1];
+                        if (parsedItemDate[2].length == 1) parsedItemDate[2] = "0" + parsedItemDate[2];
+                        item.date = parsedItemDate[0] + "-" + parsedItemDate[1] + "-" + parsedItemDate[2];
 
                         if (Date.parse(item.date) >= Date.parse("2020-03-01")) {
                             datesArr.push(item.date);
